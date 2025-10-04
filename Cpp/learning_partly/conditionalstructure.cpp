9c4d88fbd5c4a1fe8b39c67d02769a6f56f55548
Minh <quangnguyenminh2201@gmail.com>
2025-10-04 10:50:28 +0700

    setup cpp

---
+++ Cpp/learning_partly/conditionalstructure.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 10;
+    if (a < b) {
+        cout << "a is less than b" << endl;
+    } else {
+        cout << "a is not less than b" << endl;
+    }
+    return 0;
+}
